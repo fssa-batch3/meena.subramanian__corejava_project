1d5edf8a984754bcbffbc71966aa98c67800254a
vasumeenu004 <116268882+vasumeenu004@users.noreply.github.com>
2023-07-21 18:42:45 +0530

    day 10 finished

---
+++ src/main/java/day10/practice/User.java
@@ -0,0 +1,13 @@
+package day10.practice;
+
+public class User {
+	int id;
+	String name;
+	String emailId;
+
+	public User(int id, String name, String emailId) {
+		this.id = id;
+		this.name = name;
+		this.emailId = emailId;
+	}
+}
---
+++ src/main/java/day10/practice/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package day10.practice;
+
+public class UserAlreadyExistsException extends Exception {
+	 public UserAlreadyExistsException(String message) {
+	        super(message);
+	    }
+
+}
